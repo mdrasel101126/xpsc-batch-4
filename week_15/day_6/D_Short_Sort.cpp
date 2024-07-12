0bed0c16622ede6fc977a672a050cdf94da5f0b7
Rasel Miah <mdrasel101126@gmail.com>
2024-07-12 23:44:10 +0600

    Add files via upload
    day 6 added

---
+++ week_15/day_6/D_Short_Sort.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        if (s[0] == 'a' || s[1] == 'b' || s[2] == 'c')
+        {
+            cout << "YES";
+        }
+        else
+        {
+            cout << "NO";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
