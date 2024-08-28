1fc6d231a8b7662d5b4def9f47e6f717c8d808d5
Rasel Miah <mdrasel101126@gmail.com>
2024-08-28 22:28:53 +0600

    Add files via upload
    day 6 added

---
+++ week_18/day_6/A_Hayato_and_School.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> odd, even;
+        for (int i = 1; i <= n; i++)
+        {
+            int x;
+            cin >> x;
+            if (x % 2 == 0)
+            {
+                even.push_back(i);
+            }
+            else
+            {
+                odd.push_back(i);
+            }
+        }
+        if (odd.size() >= 3)
+        {
+            cout << "YES\n";
+            cout << odd[0] << " " << odd[1] << " " << odd[2] << '\n';
+        }
+        else if (odd.size() >= 1 && even.size() >= 2)
+        {
+            cout << "YES\n";
+            cout << odd[0] << " " << even[0] << " " << even[1] << '\n';
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
